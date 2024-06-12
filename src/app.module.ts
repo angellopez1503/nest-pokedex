ca6ba32b5241578e6da321748703546efd0a773f
Angel Lopez <arturolopez15@hotmail.com>
2024-06-12 10:50:35 -0500

    Preparar mongodb y envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,19 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema:JoiValidationSchema
+
+    }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
-    MongooseModule.forRoot('mongodb+srv://root:root@cluster0.wqmry8f.mongodb.net/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB,{dbName:'pokemonsdb'}),
     PokemonModule,
     CommonModule,
     SeedModule,
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -3,17 +3,22 @@ import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PockemonSchema, Pokemon } from './entities/pokemon.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
   imports:[
+    ConfigModule,
     MongooseModule.forFeature([
       {
         name:Pokemon.name,
         schema:PockemonSchema
       }
     ])
+  ],
+  exports:[
+    MongooseModule
   ]
 })
 export class PokemonModule {}
